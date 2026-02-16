dcb5d37a5c98c4c95ea9da6e131245043e7d0b30
Orim Dominic <orimdominic@gmail.com>
2026-02-16 15:10:49 +0100

    handle error cases

--- src/main.js
+++ src/main.js
@@ -2,27 +2,23 @@ const btnFetchUsers = document.getElementById("btn_fetch_users");
 
 btnFetchUsers.addEventListener("click", run);
 
-async function fetchUsers() {
-  return fetch("https://jsonplaceholder.typicode.com/users", {
-    method: "get",
-  }).then((response) => response.json());
-}
-
 async function run() {
   const usersListEl = document.getElementById("ul_users_list");
-  usersListEl.textContent = "" // clear list
+  usersListEl.textContent = ""; // clear list
 
   try {
-    const usersList = await fetch(
-      "https://jsonplaceholder.typicode.com/users",
-      {
-        method: "get",
-      },
-    ).then((response) => response.json());
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+
+    if (!response.ok) {
+      console.error(response.status);
+      throw new Error("Could not complete request");
+    }
+
+    const usersList = await response.json();
 
     usersList.forEach((user) => {
       const li = document.createElement("li");
-      li.style.paddingTop = ".5rem"
+      li.style.paddingTop = ".5rem";
       li.textContent = user.name;
       usersListEl.append(li);
     });
